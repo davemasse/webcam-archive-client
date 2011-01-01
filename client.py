5b4ec6b7bffa2c7a1730de7ec3914d6a59c9654f
Dave Masse <dmasse@rudemoose.com>
2011-01-01 12:57:02 -0500

    Update client to handle meta information.

--- client.py
+++ client.py
@@ -6,11 +6,12 @@ from wordpress_xmlrpc.wordpress import WordPressBase
 
 import base64
 import os
+import simplejson
 import sys
 
 class WebcamArchive(WordPressBase):
 	definition = {
-		'status': 'status',
+		'error': 'error',
 	}
 	
 	def __str__(self):
@@ -18,7 +19,7 @@ class WebcamArchive(WordPressBase):
 
 class WebcamArchiveClient(AuthenticatedMethod):
 	method_name = 'webcamarchive.upload'
-	method_args = ('image',)
+	method_args = ('image','meta',)
 	return_status = WebcamArchive
 
 def main(*args):
@@ -28,15 +29,24 @@ def main(*args):
 		sys.stderr.write('Please provide an image name.')
 		return 1
 	
+	try:
+		meta = simplejson.loads(args[2])
+	except IndexError:
+		meta = '{}'
+	except simplejson.decoder.JSONDecodeError:
+		sys.stderr.write('Please provide meta information in a valid JSON format.')
+		return 3
+	
 	if not os.path.exists(image):
 		sys.stderr.write('The file provided does not exist.')
 		return 2
 	
 	wp = Client(settings.WP_XMLRPC_URL, settings.WP_USERNAME, settings.WP_PASSWORD)
 	
+	# Encode image to preserve data across HTTP request
 	image_text = base64.encodestring(open(image,'rb').read())
 	
-	wp.call(WebcamArchiveClient(image_text))
+	print wp.call(WebcamArchiveClient(image_text, meta))
 	
 	return 0
 
