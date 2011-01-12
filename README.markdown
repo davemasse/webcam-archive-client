499de49b85e5a2c05a51d0dfe2ab2cc8366e0b33
Dave Masse <dmasse@rudemoose.com>
2011-01-12 15:01:26 -0500

    Update the Python client to handle a pickled Python dictionary. Update necessary documentation to reflect this change.

--- README.markdown
+++ README.markdown
@@ -8,7 +8,7 @@ Copy settings_example.py to settings.py and modify as necessary.
 
 ### Usage ###
 
-client.py -f path/to/image.jpg [-d json_meta_dict] [-v]
+client.py -f path/to/image.jpg [-d meta_dict] [-v]
 
 ### Parameters ###
 
@@ -17,7 +17,7 @@ client.py -f path/to/image.jpg [-d json_meta_dict] [-v]
    The full path to a JPEG image.
  * -d, --data
    
-   Optionally, a JSON dictionary of meta key-value pairs. The keys must be defined in the WordPress plugin configuration ahead of time. When you create a meta property, a corresponding unique "short name" reference will be generated automatically. Use these "short names" as the keys in the dictionary that you provide on the command line in the script call.
+   Optionally, a pickled Python dictionary or JSON-encoded string of meta key-value pairs. The keys must be defined in the WordPress plugin configuration ahead of time. When you create a meta property, a corresponding unique "short name" reference will be generated automatically. Use these "short names" as the keys in the dictionary that you provide on the command line in the script call.
  * -v, --verbose
    
    Verbose script output.
--- python/client.py
+++ python/client.py
@@ -7,6 +7,7 @@ from optparse import OptionParser
 
 import base64
 import os
+import pickle
 import simplejson
 import sys
 
@@ -27,25 +28,30 @@ def main():
 	usage = "Usage: %prog [options] arg"
 	parser = OptionParser(usage)
 	parser.add_option('-f', '--file', dest='image', help='Path to image file to upload')
-	parser.add_option('-d', '--data', dest='meta', help='A dictionary of meta data to upload along with the image')
+	parser.add_option('-d', '--data', dest='meta', help='A pickled Python dictionary or JSON-encoded string of meta data to upload along with the image')
 	parser.add_option('-v', '--verbose', action='store_true', dest='verbose')
 	(options, args) = parser.parse_args()
 	
 	if not hasattr(options, 'image'):
-		sys.stderr.write('Please provide an image name.')
+		sys.stderr.write('Please provide an image name.\n')
 		return 1
 	
 	if hasattr(options, 'meta'):
 		try:
-			meta = simplejson.loads(options.meta)
-		except simplejson.decoder.JSONDecodeError:
-			sys.stderr.write('Please provide meta information in a valid JSON format.')
+			# Check for Python dictionary
+			if type(pickle.loads(options.meta)) == type(dict()):
+				meta = pickle.loads(options.meta)
+			# Check for JSON-encoded string
+			else:
+				meta = simplejson.loads(options.meta)
+		except:
+			sys.stderr.write('Please provide meta information in a valid Python dictionary or JSON format.\n')
 			return 3
 	else:
 		meta = '{}'
 	
 	if not os.path.exists(options.image):
-		sys.stderr.write('The file provided does not exist.')
+		sys.stderr.write('The file provided does not exist.\n')
 		return 2
 	
 	wp = Client(settings.WP_XMLRPC_URL, settings.WP_USERNAME, settings.WP_PASSWORD)
@@ -53,10 +59,13 @@ def main():
 	# Encode image to preserve data across HTTP request
 	image_text = base64.encodestring(open(options.image,'rb').read())
 	
+	if options.verbose:
+		sys.stdout.write('Calling WordPress XML-RPC API.\n')
+	
 	status = wp.call(WebcamArchiveClient(image_text, meta))
 	
 	if options.verbose:
-		print status
+		sys.stdout.write('Return status of WordPress XML-RPC call: ' + str(status) + '\n')
 	
 	return 0
 
